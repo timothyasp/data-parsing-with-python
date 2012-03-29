f96736c1d9c7dbcab856ae1e8a1696b81e7a287d
Timothy Asp <timothy.asp@gmail.com>
2012-03-29 13:37:19 -0700

    Got the seperate vector file working

--- parser.py
+++ parser.py
@@ -2,65 +2,8 @@ import sys
 import os.path
 import csv
 import math
+from vector import Vector
 
-class Vector:
-    def __init__(self, values):
-        self.values = values
-    def dump(self):
-        print self.values
-    def length(self):
-        val = 0
-        for x in self.values:
-            x = float(x)
-            val += x*x
-        return math.sqrt(val)
-
-    def largest(self):
-        return
-    def smallest(self):
-        return
-    def mean(self):
-        return
-    def median(self):
-        return
-    def standard_dev(self):
-        return
-
-class CSVData:
-    def __init__(self, data):
-        self.data = data
-    @staticmethod
-    def largest():
-        return
-    @staticmethod
-    def smallest(vectors):
-        return
-    @staticmethod
-    def mean(vectors):
-        return
-    @staticmethod
-    def median(vectors):
-        return
-    @staticmethod
-    def standard_dev(vectors):
-        #ret = []
-        #for (vector in vectors)
-        #    ret.append(vector.standard_dev())
-        #return ret
-        return
-    @staticmethod
-    def dot_product(v1, v2):
-        return
-    @staticmethod
-    def euclidian(v1, v2):
-        return
-    @staticmethod
-    def manhattan(v1, v2):
-        return
-    @staticmethod
-    def pearson(v1, v2):
-        return
-     
 
 def main():
     if len(sys.argv) <= 1:
@@ -73,7 +16,7 @@ def main():
 
     filetype = sys.argv[1].split('.')[1];
     if filetype == 'csv':
-        parse_csv(sys.argv[1])
+        data = CSVData(parse_csv(sys.argv[1]))
     elif filetype == 'txt':
         parse_txt(sys.argv[1])
     else:
@@ -81,18 +24,18 @@ def main():
         return
     print 'Parsing file:', sys.argv[1]
 
+
 def parse_csv(filename):
     reader = csv.reader(open(filename, 'r'))
-    mylist=[]
-    j=0
+    vectors=[]
+
     for row in reader:
         for i, x in enumerate(row):
             if len(x)< 1:
                 x = row[i] = 0
-        mylist.append(Vector(list(row)))
-        print mylist[j].length()
-        mylist[j].dump()
-        j+=1
+        vectors.append(Vector(list(row)))
+
+    return vectors
 
 
 def parse_txt(filename):
@@ -101,6 +44,8 @@ def parse_txt(filename):
     #    line = file.readline()
     #    print line.split(',')
 
+def calc(data):
+    data.
 
 if __name__ == '__main__':
     main()
